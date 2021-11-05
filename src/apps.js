fa6a0b057661ad510964259312946800afeac0ba
kishozen <lazyzen@hotmail.com>
2021-11-05 01:31:31 -0600

    first commit

---
+++ src/apps.js
@@ -0,0 +1,8 @@
+const hamburgerButton = document.getElementById('hamburger')
+const navlist = document.getElementById('navList')
+
+function toggleButton(){
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click, toggleButton')
